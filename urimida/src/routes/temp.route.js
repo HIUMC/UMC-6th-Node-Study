7c7fbf2e29d11a7a5994a9868f0efd328f67fe3f
urimida <143990075+urimida@users.noreply.github.com>
2024-06-24 16:15:07 +0900

    쩨리가 지우라고 함

--- urimida/src/routes/temp.route.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { tempException } from '../controllers/temp.controller.js';
-
-export const tempRouter = express.Router();
-
-tempRouter.get('/exception/:flag',tempException);
